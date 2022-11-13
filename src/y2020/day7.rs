06dc09e29dd5b5e9d1bc4d51626f3483ebe9cc0b
Niklas Larsson <niklaslarsson95@gmail.com>
2022-11-13 22:18:28 +1100

    Day 7 part 2

--- src/y2020/day7.rs
+++ src/y2020/day7.rs
@@ -26,15 +26,13 @@ impl Day7 {
 
 impl Day for Day7 {
     fn day_name(&self) -> String { String::from("07") }
-    fn answer1(&self) -> String { String::from("?") }
-    fn answer2(&self) -> String { String::from("?") }
+    fn answer1(&self) -> String { String::from("148") }
+    fn answer2(&self) -> String { String::from("24867") }
 
     fn solve(&mut self) -> (String, String) {
         let part1 = self.find_parents("shiny gold").len();
-        let part2 = 0;
+        let part2 = self.count_children("shiny gold");
         
-        println!("{:?}", part1);
-        println!("{:?}", part2);
         (part1.to_string(), part2.to_string())
     }
 }
@@ -43,7 +41,7 @@ impl Day7 {
     fn find_parents(&self, target: &str) -> HashSet<String> {
         let parents = self.rules
             .iter()
-            .filter(|r| r.contains(target));
+            .filter(|r| r.contains_inner(target));
         let mut result = HashSet::new();
         for p in parents {
             result.insert(p.outer.to_string());
@@ -51,6 +49,24 @@ impl Day7 {
             result.extend(ancestors);
         }
         result
+    }
+
+    fn count_children(&self, target: &str) -> usize {
+        let relevant_rules = self.rules.iter()
+            .filter(|r| r.contains_outer(target));
+        
+        let mut total = 0;
+        for rule in relevant_rules {
+            // The bag
+            total += rule.quantity;
+            // Plus its children
+            if let Some(inner) = &rule.inner {
+                let bag_value = self.count_children(inner);
+                total += rule.quantity * bag_value;
+            }
+        }
+
+        total
     }
 }
 
@@ -71,14 +87,12 @@ impl Rule {
 
         let mut rules = vec![];
 
-        println!("{}", input);
         // The rest of the iterator is any number of Quantity Desc Desc bag/s,
         while let Some(quantity_s) = words.next() {
             if quantity_s.eq("no") {
                 let rule = Rule {outer: outer.trim().to_string(), inner: None, quantity: 0};
                 let _other = words.next();
                 let _bags = words.next();
-                println!("    {}", rule);
                 rules.push(rule);
             } else {
                 let quantity = quantity_s.parse::<usize>().expect("Parse error");
@@ -86,7 +100,6 @@ impl Rule {
                     + " " + words.next().expect("Error");
                 let _bag = words.next();
                 let rule = Rule {outer: outer.trim().to_string(), inner: Some(inner.trim().to_string()), quantity};
-                println!("    {}", rule);
                 rules.push(rule);
             }
         }
@@ -94,7 +107,7 @@ impl Rule {
         rules
     }
 
-    fn contains(&self, target: &str) -> bool {
+    fn contains_inner(&self, target: &str) -> bool {
         if let Some(inner) = &self.inner {
             inner.eq(target)
         } else {
@@ -102,11 +115,14 @@ impl Rule {
         }
     }
 
+    fn contains_outer(&self, target: &str) -> bool {
+        self.outer.eq(target)
+    }
+
 }
 
 impl Display for Rule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // This feels slow, but Display is only for debugging
         write!(
             f, 
             "{} -> {} {:?}",
