a7c0aa227e00dbfd3efaae6fd21dadbe77b68eb5
Niklas Larsson <niklaslarsson95@gmail.com>
2022-11-22 18:19:46 +1100

    Day 13 part 2 (plagiarism)

--- src/y2020/day13.rs
+++ src/y2020/day13.rs
@@ -1,4 +1,4 @@
-﻿use std::{fs, ops::Index};
+﻿use std::fs;
 use super::super::day::Day;
 
 pub struct Day13 {
@@ -31,9 +31,9 @@ impl Day13 {
             bus = self.buses.iter().find_map(|b_opt| {
                 if let Some(b) = b_opt {
                     if time % b == 0 { 
-                        return Some(*b)
+                        Some(*b)
                     } else { 
-                        return None;
+                        None
                     }
                 } else {
                     None
@@ -45,40 +45,53 @@ impl Day13 {
     }
 
     fn find_part_2(&self) -> usize {
-        // Apparently we need to use "Chinese Remainder Theorem" for this. Good luck, future me :)
-        let timestep: usize = self.buses.iter().filter_map(|b| *b).max().unwrap(); // Take the biggest jumps possible
-        let offset = self.buses.iter().position(|b_opt| 
-            if let Some(b) = b_opt {
-                if *b == timestep { true } else { false }
-            } else { false }
-        ).unwrap();
-        let mut time = timestep - offset; // This should mean we check the right intervals to look from left to right
-        println!("{}. Starting at {}", timestep, time);
-        'outer: loop {
-            time += timestep;
-            //println!("Checking {}", time);
-            for (i, bus) in self.buses.iter().enumerate() {
-                if let Some(b) = bus {
-                    if (time + i) % b != 0 { continue 'outer; }
-                }
-            }
 
-            break;
-        }
-        time
+        let buses = self.buses.iter().filter_map(|b| {
+            b.as_ref().copied()
+        }).collect();
+
+        let offsets = self.buses.iter().enumerate().filter_map(|(i, b)| {
+            b.as_ref().map(|bus| *bus as isize - i as isize)
+        }).collect();
+        chinese_remainder(buses, offsets)
     }
+}
+
+// This and mul_inv ported from https://0xdf.gitlab.io/adventofcode2020/13
+// Terrible naming but I don't understand enough to fix it lol. Crazy fast so idc
+fn chinese_remainder(n: Vec<usize>, a: Vec<isize>) -> usize {
+    let mut sum: isize = 0;
+    let prod: usize = n.iter().product();
+    for (n_i, a_i) in n.iter().zip(a.iter()) {
+        let p = prod / n_i;
+        sum += a_i * mul_inv(p as isize, *n_i as isize) * p as isize;
+    }
+    (sum % prod as isize) as usize
+}
+
+fn mul_inv(mut a: isize, mut b: isize) -> isize {
+    if b == 1 { return 1; }
+    let b0 = b;
+    let (mut x0, mut x1) = (0, 1);
+    while a > 1 {
+        let q = a / b;
+        (a, b) = (b, a % b);
+        (x0, x1) = (x1 - q * x0, x0);
+    }
+    if x1 < 0 { x1 += b0; }
+    x1
 }
 
 impl Day for Day13 {
     fn day_name(&self) -> String { String::from("13") }
     fn answer1(&self) -> String { String::from("2845") }
-    fn answer2(&self) -> String { String::from("?") }
+    fn answer2(&self) -> String { String::from("487905974205117") }
 
     fn solve(&mut self) -> (String, String) {
         let part1 = self.find_part_1();
         let part2 = self.find_part_2();
 
-        println!("{:?}", (part1.to_string(), part2.to_string()));
+        //println!("{:?}", (part1.to_string(), part2.to_string()));
         (part1.to_string(), part2.to_string())
     }
 }
