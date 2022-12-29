e7617aa15f2741dd6b67c825ad6e15e616d23f09
Niklas Larsson <niklaslarsson95@gmail.com>
2022-12-29 15:17:17 +1100

    Make day 15 faster

--- src/y2022/day15.rs
+++ src/y2022/day15.rs
@@ -1,14 +1,12 @@
-﻿use std::time::Instant;
-
-use rayon::prelude::{IntoParallelIterator, ParallelIterator};
+﻿use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 use super::super::day::Day;
 
 const PART1_TARGET_Y: i32 = 2000000;
 const PART2_MAX: i32 = 4000000;
-const CHUNKS: usize = 100;
+const CHUNKS: usize = 5000;
 
-/*
+/* for test data...
 const PART1_TARGET_Y: i32 = 10;
 const PART2_MAX: i32 = 20;
 const CHUNKS: usize = 10;
@@ -22,7 +20,6 @@ pub struct Day15
 }
 
 impl Day15 {
-    #[allow(dead_code)]
     pub fn new() -> Day15
     {
         let input = include_str!("input15");
@@ -30,7 +27,6 @@ impl Day15 {
 
         let scanners = input.trim().split('\n')
             .map(Scanner::from_str).collect::<Vec<Scanner>>();
-
 
         Day15 { scanners }
     }
@@ -43,92 +39,44 @@ impl Day for Day15 {
 
     fn solve(&mut self) -> (String, String)
     {
-
-        let target_row_beacons = self.scanners.iter().filter(|scanner| scanner.beacon_y == PART1_TARGET_Y).collect::<Vec<_>>();
         let coverage_ranges = self.scanners.iter().filter_map(|s| s.coverage_horizontal(PART1_TARGET_Y)).collect::<Vec<_>>();
-        //let coverage_ranges = combine_coverage_ranges(&coverage_ranges);
         // we have an iterator of ranges, find the total covered area....
         // This seems like a bad way to do this
 
         let lowest_covered = coverage_ranges.iter().min_by_key(|r| r.0).expect("?").0;
         let highest_covered = coverage_ranges.iter().max_by_key(|r| r.1).expect("?").1;
 
-        let mut covered = 0;
-        for point in lowest_covered..=highest_covered {
-            // Ignore beacons
-            if !target_row_beacons.iter().any(|b| b.beacon_x == point) 
-            && coverage_ranges.iter().any(|r| Self::check_cell(r, point)) {
-                covered += 1;
-            }
-        }
+        let covered = (lowest_covered..=highest_covered).into_par_iter().filter(|&x| 
+            coverage_ranges.iter().any(|r| Self::check_cell(r, x))
+        ).count();
         
-        let ans1 = covered;
+        let ans1 = covered - 1; // Why is this a minus 1, not minus target row beacons . len() ? not sure !
 
         let ans2 = (0..CHUNKS).into_par_iter().find_map_any(|chunk| {
             self.check_chunk(chunk)
         }).expect("Didn't find answer...");
-        println!();
 
-
-        println!("{ans1}, {ans2} {}", ans2 == 56000011);
+        //println!("{ans1}, {ans2}");
         (ans1.to_string() , ans2.to_string())
     }
-}
-
-fn combine_coverage_ranges(coverage_ranges: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
-    let mut result = vec![];
-    for range in coverage_ranges {
-        // if this range isn't covered entirely by another, include it
-        if !coverage_ranges.iter().any(|other| other.1 < range.0 && other.1 > range.0) {
-            result.push(*range);
-        }
-    }
-    result
 }
 
 impl Day15 {
     fn check_chunk(&self, my_chunk: usize) -> Option<usize> {
         let my_start = (my_chunk * CHUNK_SIZE) as i32;
         let my_end = my_start + CHUNK_SIZE as i32;
-        let before = Instant::now();
-        let result = (my_start..=my_end).find_map(|y| {
-            /*
-            if y % 100 == 0 {
-                println!("{} / {CHUNK_SIZE} ({:02.2}%)", y - my_start, ((y - my_start) as f32 * 100.0) / CHUNK_SIZE as f32);
-            }
-            */
 
-            self.check_row_3(y)
-        });
-        let elapsed = before.elapsed();
-        println!("chunk {my_chunk} took {:04} millis. {CHUNKS} chunks would take {} minutes", elapsed.as_millis(), (elapsed * CHUNKS as u32).as_secs() / 60);
-        result
+        (my_start..=my_end).find_map(|y| {
+            self.check_row(y)
+        })
     }
     
-    fn check_row(&self, check_y: i32) -> Option<i32> {
-        let coverage_ranges = self.scanners.iter().filter_map(|s| s.coverage_horizontal(check_y)).collect::<Vec<_>>();
-        let coverage_ranges = combine_coverage_ranges(&coverage_ranges);
-        for x in 0..=PART2_MAX {
-            // Ignore beacons
-            if !coverage_ranges.iter().any(|r| Self::check_cell(r, x))
-            && !self.scanners.iter().any(|scanner| scanner.beacon_y == check_y && scanner.beacon_x == x) {
-                return Some((x * 4000000) + check_y);
-            }
-        }
-        None
-    }
+    fn check_row(&self, check_y: i32) -> Option<usize> {
+        // Start with no coverage (one range of the entire row). 
+        // For each scanner, we will carve out the sections of the range that it covers
+        // Eventually, most rows will lose all their uncovered ranges.
+        // Our target row will end up with one range, that contains one point (the one possible position)
 
-    fn check_row_2(&self, check_y: i32) -> Option<i32> {
-        for x in 0..=PART2_MAX {
-            if !self.scanners.iter().any(|s| s.covers(x, check_y))
-            && !self.scanners.iter().any(|scanner| scanner.beacon_y == check_y && scanner.beacon_x == x) {
-                return Some((x * 4000000) + check_y);
-            }
-        }
-        None
-    }
-
-    fn check_row_3(&self, check_y: i32) -> Option<usize> {
         let mut uncovered_ranges = vec![(0, PART2_MAX)];
         for scanner in &self.scanners {
             let ours = scanner.coverage_horizontal(check_y);
@@ -171,11 +119,10 @@ impl Day15 {
                 new_uncovered.push((ours.1 + 1, other.1));
             }
 
-            // TODO check for beacons?
             uncovered_ranges = new_uncovered;
         }
 
-        if uncovered_ranges.len() > 0 {
+        if !uncovered_ranges.is_empty() {
             // this should be exactly one element, a one length range (x, x)
             let x = uncovered_ranges[0].0;
             Some((x as usize * 4000000) + check_y as usize)
@@ -231,10 +178,5 @@ impl Scanner {
         } else {
             None
         }
-    }
-
-    fn covers(&self, x: i32, y: i32) -> bool {
-        let x_dist = self.beacon_dist - (self.y - y).abs(); // How many steps of 'distance' do we have left to allocate to x direction?
-        x_dist > 0 && (self.x - x).abs() <= x_dist
     }
 }
