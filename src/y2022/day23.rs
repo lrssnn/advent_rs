9948903c3e427c5aebc8c3903e9433754f5f0634
Niklas Larsson <niklaslarsson95@gmail.com>
2023-01-08 16:21:02 +1100

    Paralellise day 23

--- src/y2022/day23.rs
+++ src/y2022/day23.rs
@@ -1,4 +1,6 @@
-﻿use std::{fmt::Display, collections::HashMap};
+﻿use std::fmt::Display;
+
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator, IntoParallelRefMutIterator};
 
 use super::super::day::Day;
 
@@ -22,6 +24,7 @@ impl Day23 {
         Day23 { elves, directions: Direction::default() }
     }
 
+    #[allow(unused)]
     fn print_elves(&self) {
         let min_x = self.elves.iter().map(|e| e.x).min().unwrap();
         let max_x = self.elves.iter().map(|e| e.x).max().unwrap();
@@ -67,22 +70,12 @@ impl Day23 {
 
     fn elf_has_neighbour(&self, elf: &Elf, direction: Direction) -> bool {
         let offset = direction.offset();
-        self.elves.iter().any(|other| 
-            {
-                if other.x - offset.0 == elf.x && other.y - offset.1 == elf.y {
-                    //println!("    {elf} has neighbour {other} at {direction:?}");
-                    return true;
-                }
-                return false;
-            })
+        self.elves.iter().any(|other| other.x - offset.0 == elf.x && other.y - offset.1 == elf.y)
     }
 
     fn process_turns(&mut self, turns: usize) -> usize {
         for _turn in 1..=turns {
             let no_moves = self.process_turn();
-
-            //println!("After turn {_turn}");
-            //self.print_elves();
 
             if no_moves { return _turn; }
             self.directions.reverse();
@@ -97,42 +90,34 @@ impl Day23 {
     }
 
     fn process_turn(&mut self) -> bool {
-        let mut proposals: HashMap<(isize, isize), Elf> = HashMap::new();
-        'elf_loop: for elf in &self.elves {
+        let proposals: Vec<(Elf, (isize, isize))> = self.elves.par_iter().filter_map(|elf| {
             if !self.elf_has_any_neighbour(elf) {
-                //println!("Elf {elf} has no neighbours");
-                continue;
+                return None;
             }
             for dir_set in &self.directions {
-                //println!("Elf {elf} checking {dir_set:?}");
                 if dir_set.iter().all(|&dir| !self.elf_has_neighbour(elf, dir)) {
                     let offset = dir_set[0].offset();
                     let target = (elf.x + offset.0, elf.y + offset.1);
-                    //println!("Can go: {elf} -> {},{}", target.0, target.1);
-                    if proposals.contains_key(&target) {
-                        // Remove that one and do not add this one
-                        //println!("   Someone else wants to go there, removing...");
-                        proposals.remove(&target);
-                    } else {
-                        // First proposed move to this location
-                        proposals.insert(target, *elf);
-                    }
-                    continue 'elf_loop;
+                    return Some((*elf, target));
                 }
             }
-        }
+            return None;
+        }).collect();
 
-        ////println!("Proposals: ");
-        //println!("{proposals:?}");
+        // Deduplicate proposals aiming for the same place
+        let proposals: Vec<(Elf, (isize, isize))> = proposals.par_iter().filter(|(_, pos)| 
+            proposals.iter().filter(|(_,other)| other.0 == pos.0 && other.1 == pos.1).count() == 1)
+            .cloned().collect();
+
         if proposals.is_empty() { return true; }
 
         // Apply the moves
-        for elf in self.elves.iter_mut() {
-            if let Some((pos, _)) = proposals.iter().find(|(_, other)| other.x == elf.x && other.y == elf.y) {
+        self.elves.par_iter_mut().for_each(|elf| {
+            if let Some((_, pos)) = proposals.iter().find(|(other, _)| other.x == elf.x && other.y == elf.y) {
                 elf.x = pos.0;
                 elf.y = pos.1;
             }
-        }
+        });
 
         return false;
     }
@@ -193,10 +178,7 @@ struct Elf {
 impl Elf {
     fn is_adjacent_to(&self, other: &Elf) -> bool {
         // We assume we will not be called with ourself
-        //println!("{self} checking {other} -> {} on x, {} on y", self.x.abs_diff(other.x), self.y.abs_diff(other.y));
-        let adjacent = self.x.abs_diff(other.x) <= 1 && self.y.abs_diff(other.y) <= 1;
-        //println!("is {self} adjacent to {other} ? {adjacent}");
-        adjacent
+        self.x.abs_diff(other.x) <= 1 && self.y.abs_diff(other.y) <= 1
     }
 }
 
