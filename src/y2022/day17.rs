35d2d0fed4e5a9b1e26d56a52e23fd2ef0e84d17
Niklas Larsson <niklaslarsson95@gmail.com>
2023-11-11 23:15:33 +1100

    What a mess
    Day 24 is completing but the answer is way too high
    I think it is finding suboptimal paths to places
    and then the duplicate checker is throwing away better paths...

--- src/y2022/day17.rs
+++ src/y2022/day17.rs
@@ -4,7 +4,6 @@ use super::super::day::Day;
 
 pub struct Day17
 {
-    jets: Vec<Jet>,
     chamber: Chamber,
 }
 
@@ -12,25 +11,25 @@ impl Day17 {
     pub fn new() -> Day17
     {
         let input = include_str!("input17");
+        //let input = include_str!("2022-17.txt"); // Uncle Scientist's
         //let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-
-        let jets = input.chars()
-            .map(Jet::from_char).collect::<Vec<_>>();
 
         let chamber = Chamber::new(input.chars().collect());
 
-        Day17 { jets, chamber }
+        Day17 { chamber }
     }
 }
 
 impl Day for Day17 {
     fn day_name(&self) -> String { String::from("17") }
-    fn answer1(&self) -> String { String::from("3085") }
-    //fn answer1(&self) -> String { String::from("3068") } // Example
+    //fn answer1(&self) -> String { String::from("3085") }
+    //fn answer2(&self) -> String { String::from("?") }
+    fn answer1(&self) -> String { String::from("3068") } // Example
     //fn answer2(&self) -> String { String::from("1514285714288") } // Example
-    fn answer2(&self) -> String { String::from("?") }
+    fn answer2(&self) -> String { String::from("1582758620701") } // Uncle's
 
     fn part1(&mut self) -> String {
+        return "".to_string();
         let mut part1 = self.chamber.clone();
 
         for _ in 0..2022 {
@@ -38,17 +37,10 @@ impl Day for Day17 {
         }
 
         return part1.height().to_string();
-        // let mut game = Game::new(self.jets.clone());
-
-        // for _turn in 1..=2022 {
-        //     game.run_rock();
-        // }
-
-        // game.height().to_string()
     }
 
     fn part2(&mut self) -> String {
-let mut part2 = self.chamber.clone();
+        let mut part2 = self.chamber.clone();
 
         // state will be: current piece number, current jet index, top 4 rows of chamber
         // if we get a repeat, then we found a cycle
@@ -60,7 +52,7 @@ let mut part2 = self.chamber.clone();
         let mut cycle_finder = HashMap::new();
 
         // map state to (height, drops)
-        cycle_finder.insert((part2.piecenum, part2.jetnum, 0usize), (0usize, 0usize));
+        cycle_finder.insert((part2.piecenum, part2.jetnum, 0u64), (0usize, 0usize));
 
         let mut drops = 0;
         loop {
@@ -76,293 +68,47 @@ let mut part2 = self.chamber.clone();
                 | ((part2.rocks[height - 3] as usize) << 8)
                 | (part2.rocks[height - 4] as usize);
 
-            if let Some(entry) = cycle_finder.get(&(part2.piecenum, part2.jetnum, shape)) {
-                // println!("piece = {}", part2.piecenum);
-                // println!("jetnum = {}", part2.jetnum);
-                // println!("shape = {}", shape);
+            let skyline = u64::from_ne_bytes(part2.rocks[part2.rocks.len() - 8..].try_into().unwrap());
 
-                // println!("drops until start of loop = {}", entry.1);
-                // println!("height of tower when the loop started = {}", entry.0);
+            if let Some(entry) = cycle_finder.get(&(part2.piecenum, part2.jetnum, skyline)) {
+                println!("piece = {}", part2.piecenum);
+                println!("jetnum = {}", part2.jetnum);
+                println!("shape = {}", shape);
+
+                println!("drops until start of loop = {}", entry.1);
+                println!("height of tower when the loop started = {}", entry.0);
                 let delta_height = height - entry.0;
                 let delta_drops = drops - entry.1;
-                // println!(
-                // "There is an increase of {delta_height} rows for every {delta_drops} drops"
-                // );
+                println!(
+                "There is an increase of {delta_height} rows for every {delta_drops} drops"
+                );
                 let remaining_drops = Chamber::OUCH - entry.1;
-                // println!("There are still {remaining_drops} left to go");
+                println!("There are still {remaining_drops} left to go");
 
                 let needed_drops = remaining_drops / delta_drops;
                 let leftover_drops = remaining_drops % delta_drops;
                 let integral_height = entry.0 + delta_height * needed_drops;
 
-                // println!(
-                // "The height will reach {integral_height} but there are still {leftover_drops} drops left"
-                // );
+                println!(
+                "The height will reach {integral_height} but there are still {leftover_drops} drops left"
+                );
 
                 for _ in 0..leftover_drops {
                     part2.drop_one();
                 }
                 let leftover_height = part2.height() - height;
-                // println!("After {leftover_drops} more drops, we added {leftover_height} rows");
+                println!("After {leftover_drops} more drops, we added {leftover_height} rows");
+                let answer = integral_height + leftover_height;
+                if (answer == 1514534883742) { println!("Known bad answer!!!\n");}
 
-                return (integral_height + leftover_height).to_string();
+                return (answer).to_string();
             } else {
-                cycle_finder.insert((part2.piecenum, part2.jetnum, shape), (height, drops));
+                cycle_finder.insert((part2.piecenum, part2.jetnum, skyline), (height, drops));
             }
-        }
-    }
-        /*
-        let mut game = Game::new(self.jets.clone());
-
-        let mut seen_states: HashMap<State, (usize, usize)> = HashMap::new();
-
-        const TURNS_TARGET: usize = 1_000_000_000_000;
-
-        let mut turn = 0;
-        loop {
-            game.run_rock();
-            turn += 1;
-            let height = game.height();
-            if height < 4 {
-                continue;
-            }
-
-            let state = game.get_state();
-            if let Some((turn_last_seen, height_last_seen)) = seen_states.get(&state) {
-                let cycle_turns = turn - turn_last_seen;
-                let cycle_height = height - height_last_seen;
-
-                println!("The turn is {turn}, last saw it at {turn_last_seen} ({cycle_turns} turns ago)");
-
-                let remaining_turns = TURNS_TARGET - turn_last_seen;
-
-                let needed_cycles = remaining_turns / cycle_turns;
-
-                let remainder_turns = remaining_turns % cycle_turns;
-
-                println!("{remainder_turns} left over");
-
-                let skipped_growth = cycle_height * needed_cycles;
-
-                println!("Grew {cycle_height} per cycle: {} total growth", skipped_growth);
-
-                println!("Processing {remainder_turns} remaining turns");
-                for _turn in 0..remainder_turns {
-                    game.run_rock();
-                }
-
-                let leftover_height = game.height();// - height;
-
-                let answer = height_last_seen + skipped_growth + leftover_height;
-
-                println!("example correct?: {}", answer == 1514285714288);
-                println!("larger than known too small (real)?: {}", answer > 1514534883741);
-                println!("equals known too small (real)?: {}", answer == 1514534883741);
-                println!("equals known bad (real)?: {}", answer == 1514534883742);
-                return answer.to_string();
-            } else {
-                seen_states.insert(state, (turn, height));
-            }
-        }
-    }
-    */
-}
-
-impl Day17 {
-}
-
-// For detecting cycles
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct State { 
-    spawn_counter: usize,
-    jet_counter: usize,
-    rows_top: [u8; 4],
-}
-
-struct Game {
-    rows: Vec<u8>,
-    spawn_counter: usize,
-    jets: Vec<Jet>,
-    jet_counter: usize,
-}
-
-impl Game {
-    const PIECES: [[u8; 4]; 5] = [
-        [0b0000000, 0b0000000, 0b0000000, 0b0011110], // Horizontal
-        [0b0000000, 0b0001000, 0b0011100, 0b0001000], // Plus
-        [0b0000000, 0b0000100, 0b0000100, 0b0011100], // Corner
-        [0b0010000, 0b0010000, 0b0010000, 0b0010000], // Vertical
-        [0b0000000, 0b0000000, 0b0011000, 0b0011000], // Square
-    ];
-
-    fn new(jets: Vec<Jet>) -> Game {
-        Game { rows: vec![0, 0, 0, 0, 0, 0 ,0], spawn_counter: 0, jets, jet_counter: 0 }
-    }
-
-    fn height(&self) -> usize {
-        // Make sure we aren't counting empty rows
-        let mut height = self.rows.len();
-        while height > 0 && self.rows[height - 1] == 0 {
-            height -= 1;
-        }
-        height
-    }
-
-    fn run_rock(&mut self) {
-        let mut piece = Self::PIECES[self.spawn_counter];
-        self.spawn_counter = (self.spawn_counter + 1) % 5;
-
-        // Make room at the top for the new piece
-        let mut last = self.rows.len() - 7; // Need to have 7 empty spaces
-        while self.rows[last] != 0 {
-            self.rows.push(0);
-            last += 1;
-        }
-
-        let mut bottom = self.rows.len() - 4;
-
-        // Move and drop until we can't anymore
-        loop {
-            let jet = self.jets[self.jet_counter];
-            self.jet_counter = (self.jet_counter + 1) % self.jets.len();
-
-            match jet {
-                Jet::Left => {
-                    if self.can_go_left(bottom, &piece) {
-                        for p in piece.iter_mut() { *p  <<= 1; }
-                    }
-                },
-                Jet::Right => {
-                    if self.can_go_right(bottom, &piece) {
-                        for p in piece.iter_mut() { *p  >>= 1; }
-                    }
-                }
-            }
-
-            if bottom > 0 && self.can_go_to(bottom - 1, &piece) {
-                bottom -= 1;
-            } else {
-                break; // Time to commit the piece and move on
-            }
-        }
-
-        let mut prow = 4;
-        while prow > 0 {
-            prow -= 1;
-            self.rows[bottom] |= piece[prow];
-            bottom += 1;
-        }
-    }
-
-     fn can_go_left(&self, mut bottom: usize, piece: &[u8; 4]) -> bool {
-        let mut prow = 4;
-        while prow > 0 {
-            prow -= 1;
-            if (piece[prow] & 0x40) != 0 || (self.rows[bottom] & (piece[prow] << 1)) != 0 {
-                return false;
-            }
-            bottom += 1;
-        }
-        true
-    }
-
-    fn can_go_right(&self, mut bottom: usize, piece: &[u8; 4]) -> bool {
-        let mut prow = 4;
-        while prow > 0 {
-            prow -= 1;
-            if (piece[prow] & 0x01) != 0 || (self.rows[bottom] & (piece[prow] >> 1)) != 0 {
-                return false;
-            }
-            bottom += 1;
-        }
-        true
-    }
-
-    fn can_go_to(&self, mut bottom: usize, piece: &[u8; 4]) -> bool {
-        let mut prow = 4;
-        while prow > 0 {
-            prow -= 1;
-            if (self.rows[bottom] & piece[prow]) != 0 {
-                return false;
-            }
-            bottom += 1;
-        }
-        true
-    }
-    fn spawn(&mut self) {
-    }
-
-    fn tick(&mut self) -> bool {
-        false
-    }
-
-    fn apply_jet(&mut self) {
-    }
-
-    fn apply_jet_left(&mut self) {
-    }
-
-    fn apply_jet_right(&mut self) {
-    }
-
-    fn apply_gravity(&mut self) {
-    }
-
-    fn get_state(&self) -> State {
-        let height = self.height();
-        State {
-            spawn_counter: self.spawn_counter,
-            jet_counter: self.jet_counter,
-            rows_top: [
-                self.rows[height - 1],
-                self.rows[height - 2],
-                self.rows[height - 3],
-                self.rows[height - 4],
-            ],
         }
     }
 }
 
-#[derive(Clone, Copy)]
-enum Jet {
-    Left,
-    Right
-}
-
-impl Jet {
-    fn from_char(input: char) -> Jet {
-        match input {
-            '<' => Self::Left,
-            '>' => Self::Right,
-            _ => panic!("Invalid char"),
-        }
-    }
-}
-
-impl Display for Jet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            Self::Left => '<',
-            Self::Right => '>',
-        })
-    }
-}
-
-/*
-impl Display for Game {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in &self.rows {
-            write!(f, "|").unwrap();
-            for cell in row {
-                write!(f, "{cell}").unwrap();
-            }
-            writeln!(f, "|").unwrap();
-        }
-        write!(f, "+-------+").unwrap();
-        Ok(())
-    }
-}
-*/
 #[derive(Default, Clone)]
 struct Chamber {
     jets: Vec<char>,
@@ -485,31 +231,5 @@ impl Chamber {
             top -= 1;
         }
         top
-    }
-
-    fn _print_piece(piece: &[u8; 4]) {
-        for p in piece {
-            Self::_print_row(*p);
-            println!();
-        }
-    }
-
-    fn _print_row(row: u8) {
-        let mut bit = 0x40;
-        while bit > 0 {
-            print!("{}", if (bit & row) != 0 { '#' } else { '.' });
-            bit >>= 1;
-        }
-    }
-
-    fn _draw(&self) {
-        let mut top = self.rocks.len();
-        while top > 0 {
-            top -= 1;
-            print!("|");
-            Self::_print_row(self.rocks[top]);
-            println!("|");
-        }
-        println!("+-------+");
     }
 }
--- src/y2022/day24.rs
+++ src/y2022/day24.rs
@@ -1,12 +1,14 @@
-﻿use std::{fmt::Display, ops::Add};
+﻿use std::{fmt::Display, ops::Add, io, collections::{HashMap, HashSet}, time::Instant};
+use std::hash::{Hash, Hasher};
 
 use super::super::day::Day;
 
-//const HEIGHT: usize = 34;
-//const WIDTH: usize = 100;
+const HEIGHT: u8 = 35;
+const WIDTH: u8 = 100;
+const CYCLE: usize = HEIGHT as usize * WIDTH as usize;
 
-const HEIGHT: usize = 4;
-const WIDTH: usize = 6;
+//const HEIGHT: u8 = 4;
+//const WIDTH: u8 = 6;
 
 const TARGET: Coord = Coord { x: WIDTH, y: HEIGHT}; // Target the cell above, the rules treat the actual target as a wall
 
@@ -19,16 +21,16 @@ impl Day24 {
     pub fn new() -> Day24
     {
         let input = include_str!("input24");
-        let input = include_str!("input24_example");
+        //let input = include_str!("input24_example");
 
 
         let mut blizzards = Vec::new();
 
         let lines = input.lines();
-        for (y, line) in lines.enumerate().skip(1).take(HEIGHT) {
-            for (x, c) in line.chars().enumerate().skip(1).take(WIDTH) {
+        for (y, line) in lines.enumerate().skip(1).take(HEIGHT as usize) {
+            for (x, c) in line.chars().enumerate().skip(1).take(WIDTH as usize) {
                 if c != '.' {
-                   blizzards.push(Blizzard::new(x, y, c));
+                   blizzards.push(Blizzard::new(x as u8, y as u8, c));
                 }
             }
         }
@@ -43,12 +45,10 @@ impl Day for Day24 {
     fn answer2(&self) -> String { String::from("?") }
 
     fn part1(&mut self) -> String {
-        return "".to_string();
         println!("{}x{}", WIDTH, HEIGHT);
         println!("target = {TARGET}");
-        let initial = State{ player: Coord::new(1, 0), blizzards: self.blizzards.clone() };
-        //initial.find_path().expect("Didn't Find Any Path!").to_string()
-        initial.find_path_2().to_string()
+        let initial = State { player: Coord::new(1, 0), timestamp: 0 };
+        initial.find_path_2(self.blizzards.clone()).to_string()
     }
 
     fn part2(&mut self) -> String {
@@ -56,74 +56,173 @@ impl Day for Day24 {
     }
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone)]
 struct State {
-    blizzards: Vec<Blizzard>,
     player: Coord,
+    timestamp: usize,
 }
 
-impl State {
-    fn next_states(&self) -> Vec<State> {
-        //println!("From:\n{self}\n");
-        let next_blizzards = self.next_blizzards();
-        let productives: Vec<_> = Direction::all().iter().filter_map(|dir| self.try_move(dir, &next_blizzards)).collect();
+impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.player.hash(state);
+        (self.timestamp % CYCLE as usize).hash(state);
+    }
+}
 
-        if !productives.is_empty() {
-            productives
-        } else if self.can_wait(&next_blizzards) {
-            // if we can't move towards the goal, we might want to wait
-            vec![ State { player: self.player, blizzards: self.next_blizzards() }]
-        } else {
-            // We have to retreat
-            Direction::backwards().iter().filter_map(|dir| self.try_move(dir, &next_blizzards)).collect()
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        let my_ts = self.timestamp % CYCLE;
+        let other_ts = other.timestamp % CYCLE;
+        self.player == other.player 
+        && my_ts == other_ts
+    }
+}
+
+impl Eq for State {}
+
+impl State {
+    fn next_states(&self, blizzards: &mut BlizzardContainer) -> Vec<State> {
+        //println!("From:\n{self}\n");
+        let ts = self.timestamp + 1;
+        let next_blizzards = blizzards.get(ts);
+        let mut moves: Vec<_> = Direction::all().iter().filter_map(|dir| self.try_move(dir, &next_blizzards)).collect();
+        if self.can_wait(&next_blizzards) {
+            moves.push(State { player: self.player, timestamp: ts });
         }
+        // for m in &moves {
+        //     println!("Could go\n{m}");
+        // }
+        // let mut s = String::new();
+        // io::stdin().read_line(&mut s);
+        moves
+
+        // if !moves.is_empty() {
+        //     moves
+        // } else if self.can_wait(&next_blizzards) {
+        //     // if we can't move towards the goal, we might want to wait
+        //     vec![ State { player: self.player, blizzards: self.next_blizzards() }]
+        // } else {
+        //     Vec::new()
+        // }
     }
 
     fn try_move(&self, dir: &Direction, blizzards: &[Blizzard]) -> Option<State> {
         let proposed = self.player + dir.offset();
         if proposed.x == 0 || proposed.y == 0 || proposed.x == WIDTH + 1 || proposed.y == HEIGHT + 1 { return None; }
         if blizzards.iter().all(|blizz| blizz.loc != proposed) {
-            let new_state = State { player: proposed, blizzards: blizzards.to_vec() };
+            let new_state = State { player: proposed, timestamp: self.timestamp + 1 };
             Some(new_state)
         } else {
             None
         }
     }
 
-    fn next_blizzards(&self) -> Vec<Blizzard> {
-        self.blizzards.iter().map(|blizzard| blizzard.next_loc()).collect()
-    }
 
-    fn find_path(&self) -> Option<usize> {
-        //println!("Find Path: {}", self.player);
-        if self.player == TARGET { return Some(1); } // The target is the cell above the exit, it will take us 1 more step to exit
-        if let Some(dist) = self.next_states().iter().filter_map(|s| s.find_path()).min() {
-            Some(dist + 1)
-        } else {
-            None
-        }
-    }
-
-    fn find_path_2(&self) -> usize {
+    fn find_path_2(&self, initial_state: Vec<Blizzard>) -> usize {
         let mut to_check: Vec<(State, usize)> = vec![(self.clone(), 0)];
+        let mut seen = HashSet::new();
+        let mut best_answer = usize::MAX;
+        let mut turns = 0;
+        let mut last_iteration = Instant::now();
+        let mut duplicate_skips = 0;
+        let mut distance_skips = 0;
+        let mut s = String::new();
+        let mut blizzards = BlizzardContainer::new(initial_state);
         while let Some((checking, dist)) = to_check.pop() {
-            if checking.player == TARGET { return dist + 1; } // The target is the cell above the exit, we need to take 1 more step
-            let next_states = checking.next_states();
-            let with_distance = next_states.into_iter().map(|state| (state, dist + 1));
-            to_check.extend(with_distance);
-            to_check.dedup_by_key(|e| e.0.clone());
-            println!("{} states to check", to_check.len());
+            turns += 1;
+            let remaining_dist = ((TARGET.x - checking.player.x) + (TARGET.y - checking.player.y)) as usize;
+            if dist + remaining_dist >= best_answer {
+                distance_skips += 1;
+            } else 
+            if seen.contains(&checking) {
+                duplicate_skips += 1;
+            } else {
+                seen.insert(checking.clone());
+                //println!("Checking \n{checking}");
+                if checking.player == TARGET { 
+                    // println!("{checking}");
+                    // println!("Press to continue...");
+                    // io::stdin().read_line(&mut s);
+                    best_answer = usize::min(best_answer, dist + 1); // The target is the cell above the exit, we need to take 1 more step
+                    //println!("Best answer: {best_answer}");
+                } else {
+                    //println!("From:");
+                    //checking.print(&mut blizzards);
+                    let next_states = checking.next_states(&mut blizzards);
+                    // for s in next_states.clone() {
+                    //     println!("Could go to....");
+                    //     s.print(&mut blizzards);
+                    // }
+                    // io::stdin().read_line(&mut s);
+                    let with_distance = next_states.into_iter().map(|state| (state, dist + 1));
+                    to_check.extend(with_distance);
+                    //to_check.dedup_by_key(|e| e.0.clone());
+                    if turns % 1000 == 0 {
+                        println!("{} states to check | {duplicate_skips} dups | {distance_skips} abandoned | {} best answer | {}ms per iteration", to_check.len(), best_answer, last_iteration.elapsed().as_millis());
+                        last_iteration = Instant::now();
+                    }
+                }
+            }
         }
 
-        panic!("Didn't find a path!");
+        println!("Best answer should not be 152606: {}", best_answer != 152606);
+        println!("Best answer should be smaller than 88205: {}", best_answer < 88205);
+        return best_answer;
     }
 
     fn can_wait(&self, next_blizzards: &[Blizzard]) -> bool {
         next_blizzards.iter().all(|blizz| blizz.loc != self.player)
     }
+
+    fn print(&self, blizzards: &mut BlizzardContainer) {
+        println!("t =  {}", self.timestamp);
+        // Top wall
+        for x in 0..=WIDTH {
+            if self.player.x == x && self.player.y == 0 {
+                print!("E");
+            }
+            else if x == 1 {
+                print!(".");
+            }
+            else {
+                print!("#");
+            }
+        }
+        println!("#");
+
+        // Main rows
+        for y in 1..=HEIGHT {
+            print!("#");
+            for x in 1..=WIDTH {
+                if self.player.x == x && self.player.y == y {
+                    print!("E");
+                }
+                else if let Some(b) = blizzards.get(self.timestamp).iter().find(|blizzard| blizzard.loc == (x, y)) {
+                     print!("{b}");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("#");
+        }
+
+        // Bottom wall
+        for x in 0..=WIDTH {
+            if self.player.x == x && self.player.y == HEIGHT + 1 {
+                print!("E");
+            }
+            else if x == WIDTH {
+                print!(".");
+            }
+            else {
+                print!("#");
+            }
+        }
+        println!("#")
+     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Direction {
     Up, Down, Left, Right,
 }
@@ -161,42 +260,62 @@ impl Direction {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Blizzard {
     loc: Coord,
     btype: Direction,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct BlizzardContainer {
+    blizzards: Vec<Vec<Blizzard>>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Coord {
-    x: usize,
-    y: usize,
+    x: u8,
+    y: u8,
 }
 
 impl Coord {
-    fn new(x: usize, y: usize) -> Coord {
+    fn new(x: u8, y: u8) -> Coord {
         Coord { x, y }
     }
 }
 
-impl PartialEq<(usize, usize)> for Coord {
-    fn eq(&self, other: &(usize, usize)) -> bool {
+impl PartialEq<(u8, u8)> for Coord {
+    fn eq(&self, other: &(u8, u8)) -> bool {
         self.x == other.0 && self.y == other.1
     }
 }
 
 impl Add<(isize, isize)> for Coord {
     fn add(self, rhs: (isize, isize)) -> Self {
-        let x = ((self.x as isize) + rhs.0).max(0) as usize;
-        let y = ((self.y as isize) + rhs.1).max(0) as usize;
+        let x = ((self.x as isize) + rhs.0).max(0) as u8;
+        let y = ((self.y as isize) + rhs.1).max(0) as u8;
         Coord { x, y }
     }
 
     type Output = Self;
 }
 
+impl BlizzardContainer {
+    fn new(initial_state: Vec<Blizzard>) -> BlizzardContainer {
+        BlizzardContainer { blizzards: vec![initial_state] }
+    }
+
+    fn get(&mut self, timestamp: usize) -> Vec<Blizzard> {
+        let timestamp = timestamp % CYCLE;
+        while timestamp >= self.blizzards.len() {
+            println!("Generating blizzard for {timestamp}");
+            self.blizzards.push(Blizzard::next_blizzards(&self.blizzards[self.blizzards.len() - 1]));
+        }
+        self.blizzards[timestamp].clone()
+    }
+}
+
 impl Blizzard {
-    fn new(x: usize, y: usize, c: char) -> Blizzard {
+    fn new(x: u8, y: u8, c: char) -> Blizzard {
         Blizzard { loc: Coord::new(x, y), btype: Direction::from_char(c) }
     }
 
@@ -207,6 +326,10 @@ impl Blizzard {
         if next.y == 0 { next.y = HEIGHT; }
         if next.y == HEIGHT + 1 { next.y = 1;}
         Blizzard { btype: self.btype, loc: next}
+    }
+
+    fn next_blizzards(bzs: &[Blizzard]) -> Vec<Blizzard> {
+        bzs.iter().map(|blizzard| blizzard.next_loc()).collect()
     }
 }
 
@@ -230,53 +353,5 @@ impl Display for Direction {
 impl Display for Coord {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({},{})", self.x, self.y)
-    }
-}
-
-impl Display for State {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // Top wall
-        for x in 0..=WIDTH {
-            if self.player.x == x && self.player.y == 0 {
-                write!(f, "E").unwrap();
-            }
-            else if x == 1 {
-                write!(f, ".").unwrap();
-            }
-            else {
-                write!(f, "#").unwrap();
-            }
-        }
-        writeln!(f, "#").unwrap();
-
-        // Main rows
-        for y in 1..=HEIGHT {
-            write!(f, "#");
-            for x in 1..=WIDTH {
-                if self.player.x == x && self.player.y == y {
-                    write!(f, "E").unwrap();
-                }
-                else if let Some(b) = self.blizzards.iter().find(|blizzard| blizzard.loc == (x, y)) {
-                    write!(f, "{b}");
-                } else {
-                    write!(f, ".");
-                }
-            }
-            writeln!(f, "#");
-        }
-
-        // Bottom wall
-        for x in 0..=WIDTH {
-            if self.player.x == x && self.player.y == HEIGHT + 1 {
-                write!(f, "E").unwrap();
-            }
-            else if x == WIDTH {
-                write!(f, ".").unwrap();
-            }
-            else {
-                write!(f, "#").unwrap();
-            }
-        }
-        writeln!(f, "#")
     }
 }
