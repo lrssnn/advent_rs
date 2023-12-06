f5603eba14023cf49c457e5d367a889b36f94930
Niklas Larsson <niklaslarsson95@gmail.com>
2023-12-06 17:54:50 +1100

    Day 06 and WIP optimising day 05

--- src/y2023/day05.rs
+++ src/y2023/day05.rs
@@ -18,7 +18,7 @@ impl Day05 {
 
         let (first, rest) = input.split_once("\n\n").unwrap();
         let seeds = first[7..].split(' ').map(|n| n.parse().unwrap()).collect();
-        let maps = rest.split("\n\n").map(|m| Map::from_str(m)).collect();
+        let maps = rest.split("\n\n").map(Map::from_str).collect();
 
         Day05 { seeds, maps }
     }
@@ -26,10 +26,13 @@ impl Day05 {
 
 impl Day for Day05 {
     fn day_name(&self) -> String { String::from("05") }
-    fn answer1(&self) -> String { String::from("324724204") }
-    fn answer2(&self) -> String { String::from("104070862") }
+    // fn answer1(&self) -> String { String::from("324724204") }
+    // fn answer2(&self) -> String { String::from("104070862") }
+    fn answer1(&self) -> String { String::from("35") }
+    fn answer2(&self) -> String { String::from("46") }
 
     fn part1(&mut self) -> String {
+        let _test_combined = Map::from_maps(&self.maps[0], &self.maps[1]);
         self.seeds.iter().map(|s|
             chained_map(*s, &self.maps)
         ).min().unwrap().to_string()
@@ -62,12 +65,14 @@ fn chained_map(seed: usize, maps: &[Map]) -> usize {
     maps.iter().fold(seed, |value, map| map.map(value))
 }
 
+#[derive(Clone)]
 struct Map {
     src: String,
     dest: String, 
     ranges: Vec<MapRange>,
 }
 
+#[derive(Clone, Copy)]
 struct MapRange {
     src: usize,
     dest: usize,
@@ -80,6 +85,39 @@ impl Map {
         let (src, dest) = first[..(first.len() - 5)].split_once("-to-").unwrap();
         let ranges = rest.lines().map(MapRange::from_str).collect();
         Self { src: src.to_string(), dest: dest.to_string(), ranges }
+    }
+
+    fn from_maps(a: &Map, b: &Map) -> Map {
+        // TODO TODO TODO
+        // For Each range in b, how do we reach it? 
+        // For each range that a maps to, if it would be mapped by b, split it off into
+        // its own range that combines b
+        println!("\n{a}");
+        println!("\n{b}");
+
+        let mut candidate = a.clone();
+        candidate.dest = b.dest.clone();
+        for src_range in &a.ranges {
+            println!("{src_range}");
+            println!("  This range maps from {}..{}", src_range.src, src_range.src + src_range.len);
+            println!("  This range maps into {}..{}", src_range.dest, src_range.dest + src_range.len);
+            
+            println!();
+        }
+
+        for range in &b.ranges {
+            println!("{range}");
+            println!("This range maps from {}..{}", range.src, range.src + range.len);
+            println!("The range offset is {}", range.dest as isize - range.src as isize);
+            for src_range in &a.ranges {
+                println!("  does {src_range} map into this?");
+                println!("  This range maps into {}..{}", src_range.dest, src_range.dest + src_range.len);
+
+                println!();
+            }
+        }
+
+        a.clone()
     }
 
     fn map(&self, input: usize) -> usize {
--- src/y2023/day06.rs
+++ src/y2023/day06.rs
@@ -1,29 +1,86 @@
+use std::fmt::Display;
+
 use super::super::day::Day;
 
 pub struct Day06
 {
+    races: Vec<Race>,
+    mega_race: Race,
 }
 
 impl Day06 {
     pub fn new() -> Day06
     {
-        let _input = "EXAMPLE";
-        //let input = include_str!("../../input/y2023/06");
+        //let input = "Time:      7  15   30\nDistance:  9  40  200";
+        let input = include_str!("../../input/y2023/06");
 
-        Day06 { }
+        Day06 { races: Race::from_str(input), mega_race: Race::from_str_single(input) }
     }
 }
 
 impl Day for Day06 {
-    fn day_name(&self) -> String { String::from("?? 06") }
-    fn answer1(&self) -> String { String::from("??") }
-    fn answer2(&self) -> String { String::from("??") }
+    fn day_name(&self) -> String { String::from("06") }
+    fn answer1(&self) -> String { String::from("1413720") }
+    fn answer2(&self) -> String { String::from("30565288") }
 
     fn part1(&mut self) -> String {
-        String::new()
+        self.races.iter().map(Race::count_winning_options).product::<usize>().to_string()
     }
 
     fn part2(&mut self) -> String {
-        String::new()
+        self.mega_race.count_winning_options().to_string()
+    }
+}
+
+struct Race {
+    time: usize,
+    distance: usize,
+}
+
+fn nums_from_str(input: &str) -> Vec<usize> {
+    input.split_whitespace().map(|s| s.parse::<usize>().unwrap()).collect::<Vec<_>>()
+}
+
+impl Race {
+    fn from_str(input: &str) -> Vec<Race> {
+        let (time_line, dist_line) = input.split_once('\n').unwrap();
+
+        let times = nums_from_str(&time_line[5..]);
+        let distances = nums_from_str(&dist_line[9..]);
+
+        times.iter().zip(distances).map(|(&time, distance)| Race { time, distance }).collect()
+    }
+
+    fn from_str_single(input: &str) -> Race {
+        let (time_line, dist_line) = input.split_once('\n').unwrap();
+        let time = time_line[5..].split_whitespace().collect::<String>().parse::<usize>().unwrap();
+        let distance = dist_line[9..].split_whitespace().collect::<String>().parse::<usize>().unwrap();
+
+        Race { time, distance }
+    }
+
+    fn count_winning_options(&self) -> usize {
+        // distance = charge * (time - charge)
+        // solving that for charge (quadratic formula)
+        // 0.5 * (time +- sqrt(time^2 - 4*distance))
+        let sqrt_term = ((self.time.pow(2) - 4*self.distance) as f32).sqrt();
+        let mut lower = (self.time as f32 - sqrt_term) / 2.0;
+        let mut upper = (self.time as f32 + sqrt_term) / 2.0;
+
+        // Exactly matching the distance is not considered enough, so this ensures that exact matches are rounded up
+        // (ceil 20.0 = 20, we want 21)
+        lower = lower.floor() + 1.0;
+
+        // The same logic applies here, but we would subtract one from this then add it back to compensate for the 
+        // The counting being inclusive of both ends
+        upper = upper.ceil();
+
+        (upper - lower) as usize
+    }
+}
+
+impl Display for Race {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Time: {}, Dist: {}", self.time, self.distance)
     }
 }
